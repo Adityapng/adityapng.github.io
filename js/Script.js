0fbb38de8e6ca5ed97ec25653799e479e4ef872c
Adityapng <102513627+Adityapng@users.noreply.github.com>
2023-07-11 18:42:10 +0530

    testing gsap and addednode & npm

---
+++ js/Script.js
@@ -0,0 +1,11 @@
+gsap.to("svg", { x: 100, ease: "elastic.out" });
+
+// gsap.set(".box", { y: 100, x: 90 });
+
+// const timeline = gsap.timeline();
+
+// timeline
+//   .from(".box", { x: -400, scale: 1 })
+//   .to(".box", { x: 0 })
+//   .fromTo(".box", { x: 0, scale: 2 }, { scale: 1 })
+//   .to(".box", { x: 400 });
