f9e5d59d96c8ef7b64d97cc135d171c32bfe1429
Adityapng <102513627+Adityapng@users.noreply.github.com>
2023-07-17 23:17:53 +0530

    testing nav- removed from headera nd made independent

--- js/Script.js
+++ js/Script.js
@@ -1,36 +1,56 @@
 // GSAP ScrollTrigger configuration
 gsap.registerPlugin(ScrollTrigger);
 
-function init() {
-  // Variables
-  const videoContainer = document.querySelector(".second");
-  const video = document.querySelector(".earth");
-  const videoDuration = video.duration;
-
-  // Pin the video container
-  ScrollTrigger.create({
-    trigger: videoContainer,
-    start: "top top",
-    end: "bottom bottom",
-    pin: true,
-    pinSpacing: false,
-  });
-
-  // Scrub the video based on scroll position
-  ScrollTrigger.create({
-    trigger: videoContainer,
-    start: "top top",
-    end: "bottom bottom",
-    scrub: true,
-    anticipatePin: 1,
-    // normalizeScroll: true,
-    onUpdate: (self) => {
-      const progress = self.progress;
-      video.currentTime = progress * videoDuration;
+gsap.fromTo(
+  ".container",
+  {
+    height: "100dvh",
+  },
+  {
+    height: "50px",
+    duration: 3,
+    scrollTrigger: {
+      trigger: ".container",
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+      markers: true,
+      // pin: true,
     },
-  });
-}
+  }
+);
 
-window.addEventListener("load", function () {
-  init();
-});
+// function init() {
+//   // Variables
+//   const videoContainer = document.querySelector(".second");
+//   const video = document.querySelector(".earth");
+//   const videoDuration = video.duration;
+//   console.log("asfd");
+//   console.log(video.duration);
+//   // Pin the video container
+//   ScrollTrigger.create({
+//     trigger: videoContainer,
+//     start: "top top",
+//     end: "bottom bottom",
+//     pin: true,
+//     pinSpacing: false,
+//   });
+
+//   // Scrub the video based on scroll position
+//   ScrollTrigger.create({
+//     trigger: videoContainer,
+//     start: "top top",
+//     end: "bottom bottom",
+//     scrub: true,
+//     anticipatePin: 1,
+//     // normalizeScroll: true,
+//     onUpdate: (self) => {
+//       const progress = self.progress;
+//       video.currentTime = progress * videoDuration;
+//     },
+//   });
+// }
+
+// window.addEventListener("load", function () {
+//   init();
+// });
